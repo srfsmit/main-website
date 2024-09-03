9532ff2738d6f7581b26e178ff06d2c2279c1b04
SharK-020 <dev.kunalshar0@gmail.com>
2024-09-03 21:36:31 +0530

    changed images

--- src/shared/Cover.jsx
+++ src/shared/Cover.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Main from "../images/main.jpg";
-import img1 from "../images/img1.jpg";
-import img2 from "../images/img2.jpeg";
-import img3 from "../images/img3.jpg";
-import img4 from "../images/img4.jpg";
+import img1 from "../images/1.jpg";
+import img2 from "../images/2.jpg";
+import img3 from "../images/3.jpg";
+import img4 from "../images/4.jpg";
 import img5 from "../images/img5.jpg";
 
 const Cover = () => {
@@ -36,12 +36,16 @@ const Cover = () => {
 				</div>
 			</div>
 
-			<div className="justify-between hidden col-span-5 md:flex">
-				<img src={img1} alt="1" className="w-1/6 h-auto rounded-tl-full" />
-				<img src={img2} alt="2" className="w-1/6 h-auto" />
-				<img src={img3} alt="3" className="w-1/6 h-auto" />
-				<img src={img4} alt="4" className="w-1/6 h-auto" />
-				<img src={img5} alt="5" className="w-1/6 h-auto rounded-br-full" />
+			<div className="justify-between hidden md:grid grid-cols-5 gap-4 ">
+				<img src={img1} alt="1" className="h-auto rounded-tl-full col-span-1" />
+				<img src={img2} alt="2" className="h-auto col-span-1" />
+				<img src={img3} alt="3" className="h-auto col-span-1" />
+				<img src={img4} alt="4" className="h-auto col-span-1" />
+				<img
+					src={img5}
+					alt="5"
+					className="=h-auto rounded-br-full col-span-1"
+				/>
 			</div>
 		</div>
 	);
