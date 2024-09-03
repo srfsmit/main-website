a0939abd578b8241f4997fcdc286e7069ded73fc
SharK-020 <dev.kunalshar0@gmail.com>
2024-09-03 21:52:26 +0530

    final changes

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { databases } from "../appwrite";
-import img1 from "../images/img1.jpg"; // Import your images
 import { Query } from "appwrite";
 
 const Testimonials = () => {
