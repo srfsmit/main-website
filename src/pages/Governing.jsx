e7958a48040b7bf9f3078b46c2d11ce64eee3d68
Jitam Bhardawaj <93033828+bharadwajjitam101@users.noreply.github.com>
2024-10-05 21:43:19 +0530

    Governing.jsx

--- src/pages/Governing.jsx
+++ src/pages/Governing.jsx
@@ -81,11 +81,30 @@ const Governing = () => {
 		},
 	];
 
+
+	const exMembers = [
+
+		{
+			name: "Dr Shreya Nandan",
+			position: "Former Executive Board Member",
+			tenure: "2022-2023",
+			institution: "Sikkim Manipal Institute of Medical Sciences (SMIMS)",
+		},
+		{
+			name: "Dr Kartikeya Ojha",
+			position: "President",
+			tenure: "2021-2022",
+			institution: "Sikkim Manipal Institute of Medical Sciences (SMIMS)",
+		},
+	];
+
 	return (
 		<div className="container mx-auto px-4">
 			<h1 className="text-3xl md:text-5xl font-bold text-orange-800 font-satoshi text-center mb-8">
 				Governing Council
 			</h1>
+			
+			{/* Directorate of Research section */}
 			<section className="mb-12">
 				<h2 className="text-3xl mb-4">Directorate of Research</h2>
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -118,6 +137,7 @@ const Governing = () => {
 				</div>
 			</section>
 
+			{/* Chairperson SRF section */}
 			<section className="mb-12">
 				<h2 className="text-3xl mb-4">Chairperson SRF</h2>
 				<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -140,7 +160,8 @@ const Governing = () => {
 				</div>
 			</section>
 
-			<section>
+			{/* Executive Council section */}
+			<section className="mb-12">
 				<h2 className="text-3xl mb-4">Executive Council</h2>
 				<div className="overflow-x-auto">
 					<table className="w-full mb-8 table-auto">
@@ -159,6 +180,41 @@ const Governing = () => {
 									</td>
 									<td className="px-4 py-2 border border-black">
 										{member.position}
+									</td>
+									<td className="px-4 py-2 border border-black">
+										{member.institution}
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</section>
+
+			{/* Ex-Executive Council section */}
+			<section>
+				<h2 className="text-3xl mb-4">Ex-Executive Council</h2>
+				<div className="overflow-x-auto">
+					<table className="w-full mb-8 table-auto">
+						<thead className="bg-orange-500 text-white">
+							<tr>
+								<th className="px-4 py-2">Name</th>
+								<th className="px-4 py-2">Position</th>
+								<th className="px-4 py-2">Tenure</th>
+								<th className="px-4 py-2">Institution</th>
+							</tr>
+						</thead>
+						<tbody className="text-black">
+							{exMembers.map((member, index) => (
+								<tr key={index}>
+									<td className="px-4 py-2 border border-black">
+										{member.name}
+									</td>
+									<td className="px-4 py-2 border border-black">
+										{member.position}
+									</td>
+									<td className="px-4 py-2 border border-black">
+										{member.tenure}
 									</td>
 									<td className="px-4 py-2 border border-black">
 										{member.institution}
