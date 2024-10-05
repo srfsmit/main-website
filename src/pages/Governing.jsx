ea71637ccf354184731f83f38e0c0ec931412d16
Jitam Bhardawaj <93033828+bharadwajjitam101@users.noreply.github.com>
2024-10-05 18:30:17 +0530

    Governing.jsx

--- src/pages/Governing.jsx
+++ src/pages/Governing.jsx
@@ -82,78 +82,69 @@ const Governing = () => {
 	];
 
 	return (
-		<>
-			<div className="mx-auto text-3xl font-bold text-orange-800 font-satoshi md:text-5xl">
+		<div className="container mx-auto px-4">
+			<h1 className="text-3xl md:text-5xl font-bold text-orange-800 font-satoshi text-center mb-8">
 				Governing Council
-			</div>
-			<div className="flex flex-col gap-4">
-				<div className="mx-auto text-3xl md:mx-0">Directorate of Research</div>
-				<div className="grid grid-cols-1 gap-1 md:grid-cols-3">
+			</h1>
+			<section className="mb-12">
+				<h2 className="text-3xl mb-4">Directorate of Research</h2>
+				<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
 					<div className="p-4 text-white bg-orange-500 rounded-xl">
-						<p>
-							<b>DIRECTOR, DIRECTORATE OF RESEARCH</b>
-						</p>
+						<h3 className="font-bold mb-2">DIRECTOR, DIRECTORATE OF RESEARCH</h3>
 						<p>Dr Kalpana Sharma,</p>
 						<p>Professor, Dept. of CSE - SMIT</p>
 						<p>
-							Email Id:&nbsp;director.dor@smu.edu.in / kalpana.s@smit.smu.edu.in
+							Email Id: director.dor@smu.edu.in / kalpana.s@smit.smu.edu.in
 						</p>
-						<p>Phone No: +91 9563205842 | +91&nbsp;9641580247</p>
+						<p>Phone No: +91 9563205842 | +91 9641580247</p>
 					</div>
 					<div className="p-4 text-white bg-orange-500 rounded-xl">
-						<p>
-							<b>ASSOCIATE DIRECTOR (R&amp;D) SMIT</b>
-						</p>
+						<h3 className="font-bold mb-2">ASSOCIATE DIRECTOR (R&amp;D) SMIT</h3>
 						<p>Dr. Chandrasekhar Bhuiyan,</p>
 						<p>
-							Professor &amp; HOD, Dept of&nbsp; &nbsp;Civil Engineering, SMIT
+							Professor &amp; HOD, Dept of Civil Engineering, SMIT
 						</p>
 						<p>Email Id: hod.ce@smit.smu.edu.in</p>
 						<p>Phone No. 9836562555</p>
 					</div>
 					<div className="p-4 text-white bg-orange-500 rounded-xl">
+						<h3 className="font-bold mb-2">ASSOCIATE DEAN (R&amp;D) SMIMS</h3>
 						<p>
-							<b>ASSOCIATE DEAN (R&amp;D) SMIMS</b>
-						</p>
-						<p>
-							Dr Luna Adhikari Professor &amp; HOD, Dept of Microbiology,SMIMS
+							Dr Luna Adhikari Professor &amp; HOD, Dept of Microbiology, SMIMS
 						</p>
 						<p>Email Id: luna.a@smims.smu.edu.in</p>
 						<p>Phone No: 9434864772</p>
 					</div>
 				</div>
-			</div>
+			</section>
 
-			<div className="flex flex-col gap-4">
-				<div className="mx-auto text-3xl md:mx-0">Chairperson SRF</div>
-				<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+			<section className="mb-12">
+				<h2 className="text-3xl mb-4">Chairperson SRF</h2>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 					<div className="p-4 text-white bg-orange-500 rounded-xl">
-						<p>
-							<b>Chairperson, SRF SMIT</b>
-						</p>
+						<h3 className="font-bold mb-2">Chairperson, SRF SMIT</h3>
 						<p>Dr Joydeep Biswas,</p>
 						<p>
 							Assistant Professor (Selection Grade), Dept. of Chemistry- SMIT
 						</p>
-						<p>Email Id:&nbsp;joydeep.biswas@smit.smu.edu.in</p>
+						<p>Email Id: joydeep.biswas@smit.smu.edu.in</p>
 						<p>Phone No: +91 9563318330</p>
 					</div>
 					<div className="p-4 text-white bg-orange-500 rounded-xl">
-						<p>
-							<b>Chairperson, SRF SMU</b>
-						</p>
+						<h3 className="font-bold mb-2">Chairperson, SRF SMU</h3>
 						<p>Dr. Barkha Devi,</p>
 						<p>ASSOCIATE PROFESSOR, Sikkim Manipal College of Nursing</p>
 						<p>Email Id: barkha.d@smims.smu.edu.in</p>
 						<p>Phone No. +91 7479278673</p>
 					</div>
 				</div>
-			</div>
-			<div className="flex flex-col gap-4">
-				<div className="mx-auto text-3xl md:mx-0">Executive Council</div>
-				<div className="overflow-x-auto text-white">
+			</section>
+
+			<section>
+				<h2 className="text-3xl mb-4">Executive Council</h2>
+				<div className="overflow-x-auto">
 					<table className="w-full mb-8 table-auto">
-						<thead className="bg-orange-500">
+						<thead className="bg-orange-500 text-white">
 							<tr>
 								<th className="px-4 py-2">Name</th>
 								<th className="px-4 py-2">Position</th>
@@ -161,8 +152,8 @@ const Governing = () => {
 							</tr>
 						</thead>
 						<tbody className="text-black">
-							{members.map((member) => (
-								<tr>
+							{members.map((member, index) => (
+								<tr key={index}>
 									<td className="px-4 py-2 border border-black">
 										{member.name}
 									</td>
@@ -177,8 +168,8 @@ const Governing = () => {
 						</tbody>
 					</table>
 				</div>
-			</div>
-		</>
+			</section>
+		</div>
 	);
 };
 
