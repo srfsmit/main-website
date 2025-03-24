04d8e9716bf80f1ad7b331f2053bae5d74e399ec
dia <54020066+dianosaur12@users.noreply.github.com>
2025-03-25 02:40:59 +0530

    Update Developers.jsx
    fixed minor bug

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -54,7 +54,8 @@ const DevelopersPage = () => {
 			position: "Developer & Webmaster, President, SRF SMIT (2025-2026)",
 			institution: "Sikkim Manipal Institute of Technology",
 			image: tor,
-			message: "Despite being a student of technology, web development had never really quite piqued my interest until I was asked to become the sole maintainer of this website. However, now that I've finally been made to rise to this occasion, I think web dev can be quite fun sometimes :)"
+			message: "Despite being a student of technology, web development had never really quite piqued my interest until I was asked to become the sole maintainer of this website. However, now that I've finally been made to rise to this occasion, I think web dev can be quite fun sometimes :)",
+		},
 		{
 			name: "Abhigyan Borah",
 			position: "Developer (Intern) & Member of Team of Officials, SRF SMIT (2024-2025)",
